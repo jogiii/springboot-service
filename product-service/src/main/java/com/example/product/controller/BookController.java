21c847f7adbe0f8682a5b450efa2c5f44233d1fb
jog <joginder@publicissapient.com>
2022-07-07 23:46:35 +0530

    CRUD changes

---
+++ product-service/src/main/java/com/example/product/controller/BookController.java
@@ -0,0 +1,120 @@
+package com.example.product.controller;
+
+import com.example.product.models.Book;
+import com.example.product.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/mongodbapp")
+public class BookController
+{
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/books")
+    public ResponseEntity<List> getAllBooks(@RequestParam(required = false) String bookTitle)
+    {
+        try
+        {
+            List listOfBooks = new ArrayList<>();
+            if(bookTitle == null || bookTitle.isEmpty())
+            {
+                bookRepository.findAll().forEach(listOfBooks::add);
+            }
+            else
+            {
+                bookRepository.findByTitleContaining(bookTitle).forEach(listOfBooks::add);
+            }
+
+            if(listOfBooks.isEmpty())
+            {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(listOfBooks, HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity getBookById(@PathVariable("id") String id)
+    {
+        try
+        {
+            Optional bookOptional = bookRepository.findById(id);
+
+            return new ResponseEntity<>(bookOptional.get(), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity addABookToLibrary(@RequestBody Book book)
+    {
+        try
+        {
+            Book createdBook = bookRepository.save(new Book(book.getTitle(), book.getAuthor(),
+                    book.getIsbn()));
+            return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/books/{id}")
+    public ResponseEntity updateABook(@PathVariable("id") String id, @RequestBody Book book)
+    {
+        Optional bookOptional = bookRepository.findById(id);
+
+        if(bookOptional.isPresent())
+        {
+            Book updatedBook = (Book) bookOptional.get();
+            updatedBook.setTitle(book.getTitle());
+            updatedBook.setAuthor(book.getAuthor());
+            updatedBook.setIsbn(book.getIsbn());
+            return new ResponseEntity<>(bookRepository.save(updatedBook), HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity deleteABook(@PathVariable("id") String id)
+    {
+        try
+        {
+            bookRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
